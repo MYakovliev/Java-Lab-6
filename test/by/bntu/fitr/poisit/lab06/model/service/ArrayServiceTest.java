c19985f01189a4cd4a5d4a7d1e2ec88295f5f2f4
Saviorich <defalt3874@gmail.com>
2020-12-06 13:42:35 +0300

    added tests

--- test/by/bntu/fitr/poisit/lab06/model/service/ArrayServiceTest.java
+++ test/by/bntu/fitr/poisit/lab06/model/service/ArrayServiceTest.java
@@ -27,10 +27,32 @@ class ArrayServiceTest {
     @Test
     void calculateSumAllPositive1() {
         int expected = 25;
-        int actual = 0;
+        int actual = service.calculateSumAllPositive(new int[]{-5, 2, 13, 0, 10, -2, -125});
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateSumAllPositive2() {
+        int expected = 0;
+        int actual = service.calculateSumAllPositive(new int[]{-5, -2, -13, 0, -10, -2, -125});
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void calculateSumAfterLast1() {
+        int expected = 13;
+        int actual = service.calculateSumAfterLast(new int[]{24, 0, 123, 1, 0, 5, 6, 2}, 0);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateSumAfterLast2() {
+        int expected = 0;
+        int actual = service.calculateSumAfterLast(new int[]{2, 0, 123, 1, 0, 5, 6, 2}, 2);
+
+        assertEquals(expected, actual);
     }
 }
